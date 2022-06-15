17cf6519683b49b3d1f71daab9e0441dcfb19fd8
sk8erlucas <64547626+sk8erlucas@users.noreply.github.com>
2022-06-15 10:01:43 -0300

    Add files via upload

---
+++ Ejercicio 3.1.py
@@ -0,0 +1,27 @@
+
+
+Nombre = "Lucas"
+Apellidos = "Laino"
+Edad = 33
+Email = "galileo.laino@gmail.com"
+Telefono = "1526646791"
+Casado = True
+ConHijos = True
+Listadeamigos = ["Juan", "Alexis", "Agustin"]
+
+Peliculasvistas = {
+    1: "El juego del Miedo",
+    2: "Gladiador",
+    3: "Sonic",
+    4: "Sonic 2"
+    }
+
+print(Nombre)
+print(Apellidos)
+print(Edad)
+print(Email)
+print(Telefono)
+print(Casado)
+print(ConHijos)
+print(Listadeamigos)
+print(Peliculasvistas)
