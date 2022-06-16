d5962f9a6ab503149b3daee5bc186fdfbbebe4b8
sk8erlucas <64547626+sk8erlucas@users.noreply.github.com>
2022-06-16 09:27:04 -0300

    Add files via upload

---
+++ Ejercicio 4.1.py
@@ -0,0 +1,9 @@
+
+edad = int(input("Ingrese su edad: "))
+
+if edad < 18:
+    print("Su edad es", edad, ", por lo tanto es menor")
+else:
+    print("Su edad es", edad, ", por lo tanto es mayor")
+
+
